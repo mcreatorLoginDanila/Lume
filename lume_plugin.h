ecc7804ea132837623fb8a3abd73ba51a850be71
NotAndrey <danila.elensky@gmail.com>
2026-06-10 16:27:37 +0300

    V0.6.1 + GPLv3

--- lume_plugin.h
+++ lume_plugin.h
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2026 mcreatorLoginDanila also known as NotAndrey
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * License along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef LUME_PLUGIN_H
 #define LUME_PLUGIN_H
 #include <windows.h>
@@ -24,6 +40,7 @@ extern "C" {
         void (*on_mouse_down)(void* page_context, int x, int y, int button);
         void (*on_key_down)(void* page_context, int vk);
         void (*on_mouse_move)(void* page_context, int x, int y);
+        void (*on_mouse_wheel)(void* page_context, int delta);
         void (*on_char)(void* page_context, unsigned int ch);
     } CustomPageHandler;
     struct CustomProtocolHandler {
@@ -34,6 +51,7 @@ extern "C" {
         int (*get_document_height)(void* ctx);
         void (*on_mouse_down)(void* ctx, int x, int y, int button);
         void (*on_mouse_move)(void* ctx, int x, int y);
+        void (*on_mouse_wheel)(void* ctx, int delta);
     };
     typedef struct LumeHostAPI {
         HWND(*get_main_hwnd)(void);
@@ -57,6 +75,8 @@ extern "C" {
         int (*p_lua_rawgeti)(lua_State* L, int index, lua_Integer n);
         lua_Number(*p_lua_tonumberx)(lua_State* L, int index, int* isnum);
         void (*p_lua_settop)(lua_State* L, int index);
+        void (*p_lua_createtable)(lua_State* L, int narr, int nrec);
+        void (*p_lua_setfield)(lua_State* L, int index, const char* k);
         const char* (*get_node_prop)(HTP_NodeHandle node, const char* key, const char* default_val);
         int (*get_node_prop_int)(HTP_NodeHandle node, const char* key, int default_val);
         void (*register_tag)(CustomTagHandler handler);
@@ -67,6 +87,11 @@ extern "C" {
         int (*wasm_load)(const char* bytes, size_t len);
         void (*register_on_reset)(void (*callback)());
         void (*register_protocol_engine)(CustomProtocolHandler handler);
+        int (*gl_get_context)(const char* id, HDC* out_hdc, HGLRC* out_hglrc);
+        HTP_NodeHandle(*get_dom_root)(void);
+        int (*get_node_children_count)(HTP_NodeHandle node);
+        HTP_NodeHandle(*get_node_child)(HTP_NodeHandle node, int index);
+        void (*set_node_prop)(HTP_NodeHandle node, const char* key, const char* val);
     } LumeHostAPI;
 #ifdef BUILDING_PLUGIN
 #define LUME_PLUGIN_EXPORT __declspec(dllexport)
@@ -82,6 +107,9 @@ extern "C" {
 #undef lua_pushcfunction
 #undef lua_setglobal
 #undef lua_register
+#undef lua_createtable
+#undef lua_newtable
+#undef lua_setfield
 #define luaL_checknumber(L, arg) g_api->p_luaL_checknumber(L, arg)
 #define luaL_checkinteger(L, arg) g_api->p_luaL_checkinteger(L, arg)
 #define luaL_optinteger(L, arg, def) g_api->p_luaL_optinteger(L, arg, def)
@@ -93,6 +121,9 @@ extern "C" {
 #define lua_pushcfunction(L, f) g_api->p_lua_pushcclosure(L, f, 0)
 #define lua_setglobal(L, name) g_api->p_lua_setglobal(L, name)
 #define lua_register(L, n, f) (g_api->p_lua_pushcclosure(L, f, 0), g_api->p_lua_setglobal(L, n))
+#define lua_createtable(L, narr, nrec) g_api->p_lua_createtable(L, narr, nrec)
+#define lua_newtable(L) g_api->p_lua_createtable(L, 0, 0)
+#define lua_setfield(L, idx, k) g_api->p_lua_setfield(L, idx, k)
 #else
 #define LUME_PLUGIN_EXPORT __declspec(dllimport)
 #endif
